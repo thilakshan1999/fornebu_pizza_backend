8269a325919f41a34899788b252e5266dab55c68
Kathiramalai-Thilakshan <thilakshan@techserw.com>
2025-02-23 09:49:42 +0530

    Initial commit

---
+++ src/main/java/com/redhood/fornebu_pizza_backend/FornebuPizzaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.redhood.fornebu_pizza_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornebuPizzaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FornebuPizzaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redhood/fornebu_pizza_backend/FornebuPizzaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhood.fornebu_pizza_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornebuPizzaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
